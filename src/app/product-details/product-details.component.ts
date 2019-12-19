5dfacf4eea2d242c95dded14af0be6209f5fc42f
vinoth1710 <vinoth17@hotmail.fr>
2019-12-19 14:52:20 +0100

    take back here

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { products } from '../products';
+import { CartService } from '../cart.service';
+
 
 
 @Component({
@@ -12,7 +14,13 @@ export class ProductDetailsComponent implements OnInit {
 product;
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
              ) {}
+
+addToCart(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
